c0bf00d2a25f413c0c82a7a8358c36092dfde946
Dheeraj Gupta <Dheeraj.Gupta@motorolasolutions.com>
2024-07-24 15:19:17 +0530

    db issues fixed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import asyncio
+import asyncio
 import os
 import requests
 import re
@@ -9,6 +9,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 
+import base58
 import base64
 from pydantic import BaseModel, Field, field_validator # v2 needed
 from bson import ObjectId
@@ -27,8 +28,9 @@ wallet_collection = db.wallet
 
 
 
-TOKEN: Final = '7315336925:AAHu6_EX-pmpmxl8DmjO8IBHKwAFl_IhQlM'
+TOKEN: Final = ''
 BOT_NAME: Final = '@crypto737263_bot'
+SHYFT_API_KEY = 'GlJ737ibT8z26T3f'
 chain_id = "solana"  # Change to the appropriate chain ID
 
 main_keyboard = [
@@ -61,23 +63,17 @@ class UserModel(BaseModel):
     userId: int = Field(..., unique=True)
     privateKey: str
     publicKey: str
-
-    @field_validator('privateKey', 'publicKey')
-    def check_base64(cls, v):
-        try:
-            base64.b64decode(v)
-            return v
-        except Exception as e:
-            raise ValueError("Invalid base64 encoded key")
+    keypair: str
 
     class Config:
         populate_by_name = True
         json_encoders = {ObjectId: str}
         json_schema_extra = {
             "example": {
-                "userId": 4372293,
-                "privateKey": base64.b64encode(b'some_private_key').decode('utf-8'),
-                "publicKey": base64.b64encode(b'some_public_key').decode('utf-8')
+                "userId": "3234323432",
+                "privateKey": "fgfgu47574u34433",
+                "publicKey": "ptgjndf985544",
+                "keypair": "sdfbsd8y8dsiu44",
             }
         }
         
@@ -94,6 +90,18 @@ def pubkey_to_bytes(pubkey: Pubkey) -> bytes:
 
 def keypair_to_bytes(keypair: Keypair) -> bytes:
     return keypair.secret()
+
+
+def keypair_to_base58_string(keypair: Keypair) -> str:
+    secret_key_bytes = keypair.secret()
+    base58_secret_key = base58.b58encode(secret_key_bytes).decode('utf-8')
+    return base58_secret_key
+
+def base58_string_to_keypair(base58_secret_key: str) -> Keypair:
+    secret_key_bytes = base58.b58decode(base58_secret_key)
+    keypair = Keypair.from_secret_key(secret_key_bytes)
+    return keypair
+
 
 async def insert_user(user_data: UserModel):
     try:
@@ -150,9 +158,9 @@ def delete_user(userId: str):
 
 
 # Get all users
-all_users = get_users()
-for user in all_users:
-    print(user)
+# all_users = get_users()
+# for user in all_users:
+#     print(user)
 
 async def main_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(main_keyboard)
@@ -196,8 +204,13 @@ async def button_click_callback(update: Update, context: ContextTypes.DEFAULT_TY
         else:
             print('wallet already exist')
             await send_message(chat_id, f"A wallet is already created with your account\\.\nCurrently we support only one wallet per user\nYour *Public Key*: _`{retrieved_user.publicKey}`_ \\(Tap to copy\\)", context)
+    elif callback_data == 'export_private_key':
+        retrieved_user = await get_user_by_userId(int(chat_id))
+        print('retrieved_user',retrieved_user)
+        print('private key',retrieved_user.privateKey)
+        await send_message(chat_id, f"*Private Key*: _`{retrieved_user.privateKey}`_ \\(Tap to copy\\)", context)
     elif callback_data == 'buy_0.1_sol':
-        await send_message(chat_id, f"buying 0.1 SOL", context)
+        await send_message(chat_id, f"Buying 0\\.1 SOL", context)
     elif callback_data == 'buy_x_sol':
         await send_message(chat_id, f"Please enter the amount of SOL you want to swap:", context)    
     elif callback_data == 'sell_x_percent':
@@ -251,14 +264,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             else:
                 await send_message(chat_id, f"Token information not found for address: {token_address}", context)
         elif re.match(r'^\d+(\.\d+)?$', text):
-            print('-amount', amount)
             amount = float(text)
+            print('-amount', amount)
             # pending_amount[chat_id] = amount
-            await send_message(chat_id, f"Amount set to {escape_dots(amount)} SOL.", context)
+            await send_message(chat_id, f"Buying amount set to {escape_dots(amount)} SOL", context)
         elif re.match(r'^\d+(\.\d+)?%$', text):
             percentage = float(text.strip('%'))
             print('percentage-', percentage)
-            await send_message(chat_id, f"Percentage set to {escape_dots(percentage)}\\% SOL.", context)
+            await send_message(chat_id, f"Percentage set to {escape_dots(percentage)}\\% SOL", context)
             # asyncio.run(handle_sell(chat_id, percentage))
         else:
             print('private chat replyback')
@@ -272,7 +285,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def send_message(chat_id, message, context: ContextTypes.DEFAULT_TYPE, reply_keyboard=None):
     print('-sendmsg chatId', chat_id)
     print('-sendmsg text', message)
-    await context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_keyboard, parse_mode='MarkdownV2')
+    await context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_keyboard, disable_web_page_preview=True, parse_mode='MarkdownV2')
 
 
 async def send_token_info_and_swap_menu(chat_id, token_info, token_address, context: ContextTypes.DEFAULT_TYPE):
@@ -283,8 +296,8 @@ async def send_token_info_and_swap_menu(chat_id, token_info, token_address, cont
 
     # selected_option.setdefault(chat_id, {"buy": None, "sell": None})
 
-    buy_0_1_sol_text = "0\\.1 SOL" # if selected_option[chat_id]["buy"] == "0.1_sol" else "0.1 SOL"
-    buy_0_5_sol_text = "0\\.5 SOL" # if selected_option[chat_id]["buy"] == "0.5_sol" else "0.5 SOL"
+    buy_0_1_sol_text = "0.1 SOL" # if selected_option[chat_id]["buy"] == "0.1_sol" else "0.1 SOL"
+    buy_0_5_sol_text = "0.5 SOL" # if selected_option[chat_id]["buy"] == "0.5_sol" else "0.5 SOL"
     buy_1_sol_text = "1 SOL" # if selected_option[chat_id]["buy"] == "1_sol" else "1 SOL"
 
     sell_50_text = "Sell 50%" #if selected_option[chat_id]["sell"] == "50" else "Sell 50%"
@@ -292,13 +305,12 @@ async def send_token_info_and_swap_menu(chat_id, token_info, token_address, cont
     sell_25_text = "Sell 25%" #if selected_option[chat_id]["sell"] == "25" else "Sell 25%"
 
     token_info_message = (
-        f"{token_info['symbol']} \\- {token_info['name']}\n"
+        f"{token_info['symbol']} \\- {token_info['name']} [📈](https://dexscreener.com/{chain_id}/{token_address})\n"
         f"`{token_address}` _\\(Tap to copy\\)_ \n\n"
         f"*Price \\(USD\\):* {escape_dots(token_info['price_usd'])}\n"
         f"*Liquidity \\(USD\\):* {escape_dots(token_info['liquidity_usd'])}\n"
         # f"*FDV:* {token_info['fdv']}\n\n"
-        f"[📈Visit dexscreener](https://dexscreener.com/{chain_id}/{token_address})\n"
-        f"__Choose an action__\\:"
+        # f"__Choose an action__\\:"
     )
 
     reply_keyboard = InlineKeyboardMarkup([
@@ -322,7 +334,7 @@ async def send_token_info_and_swap_menu(chat_id, token_info, token_address, cont
         ],
         [
             {"text": sell_100_text, "callback_data": "sell_100_percent"},
-            {"text": "Sell X\\%", "callback_data": "sell_x_percent"}
+            {"text": "Sell X%", "callback_data": "sell_x_percent"}
         ],
         # [
         #     {"text": "Execute", "callback_data": "execute_trade"}
@@ -332,7 +344,33 @@ async def send_token_info_and_swap_menu(chat_id, token_info, token_address, cont
     await send_message(chat_id, token_info_message, context, reply_keyboard)
 
 
-if __name__ == '__main__':
+async def insert(chat_id):
+    retrieved_user = await get_user_by_userId(int(chat_id))
+    print('retrieved_user',retrieved_user)
+    
+    if(retrieved_user==None):
+        print('------creating new user')
+        keypair = Keypair()
+        private_key = str(keypair.secret())
+        public_key = str(keypair.pubkey())
+        keypairStr = str(keypair)
+        new_user = UserModel(userId=int(123456789), privateKey=private_key, publicKey=public_key, keypair=keypairStr)
+        await insert_user(new_user)
+    else:
+        print('------userrrr already exist')
+    
+async def getAllUsers():    
+    all_users = get_users()
+    for user in all_users:
+        print(user)
+
+async def getUser(uid):    
+    # uid=894489141
+    retrieved_user = await get_user_by_userId(int(uid))
+    print('retrieved_user',retrieved_user)
+    
+     
+def main():
     print('started bot')
 
     app = Application.builder().token(TOKEN).build()
@@ -340,7 +378,15 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('main', main_command))
     app.add_handler(CallbackQueryHandler(button_click_callback))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
+    
+    # asyncio.run(insert(123456789))
+    # asyncio.run(getUser(123456789))
+    # asyncio.run(getAllUsers())
+        
     print('polling---')
-
     app.run_polling(poll_interval=3)
+
+
+
+if __name__ == '__main__':
+    main()
