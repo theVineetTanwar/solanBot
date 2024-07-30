9c995da484a3f4977276f08475d87af505866da0
Vineet <vineet@digitizeco.in>
2024-07-30 12:45:05 +0530

    added class 'solanaHelper'

--- solanaHelper.py
+++ solanaHelper.py
@@ -29,6 +29,37 @@ def getLatestBlockHash():
 def getAccountInfo(pubkey):
     return client.get_balance(pubkey)
 
-# transactionFun()
-# pubkeyfromStr1 = Pubkey.from_string("string") 
-# info = getAccountInfo(pubkeyfromStr1)
+
+
+class SolanaHelper():
+    def __init__(
+        self,
+    ):
+        """Init API client."""
+        super().__init__()
+        self.client =  Client("https://api.devnet.solana.com")
+        # self._provider = http.HTTPProvider(endpoint, timeout=timeout, extra_headers=extra_headers)
+
+    
+    def transactionFun(self, sender: Keypair, senderPubKey: Pubkey, receiver: Pubkey, amount):
+
+        txn = Transaction().add(transfer(
+            TransferParams(
+                from_pubkey=senderPubKey, to_pubkey=receiver, lamports=amount
+            )
+        ))
+        txnRes = self.client.send_transaction(txn, sender).value # doctest: +SKIP like as 3L6v5yiXRi6kgUPvNqCD7GvnEa3d1qX79REdW1KqoeX4C4q6RHGJ2WTJtARs8ty6N5cSVGzVVTAhaSNM9MSahsqw
+        # return response as URL https://solscan.io/tx/txnRes?cluster=devnet
+        return txnRes
+
+    def getLatestBlockHash(self):
+        return self.client.get_latest_blockhash()
+
+    def getAccountInfo(self, pubkey):
+        return self.client.get_balance(pubkey)
+
+
+
+# pubkeyfromStr1 = Pubkey.from_string("str") 
+# helper = SolanaHelper()
+# info = helper.getAccountInfo(pubkeyfromStr1)
