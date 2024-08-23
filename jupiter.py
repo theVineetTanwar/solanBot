e3cf48ea3f2996634dd91eb99e33b79d906dc758
Vineet <vineet@digitizeco.in>
2024-08-23 10:27:47 +0530

    implemented command for viewing orders

--- jupiter.py
+++ jupiter.py
@@ -1,6 +1,7 @@
 import base58
 import base64
 import json
+import requests
 import os
 import asyncio
 
@@ -18,100 +19,6 @@ from jupiter_python_sdk.jupiter import Jupiter, Jupiter_DCA
 import httpx
 import constant
 
-
-
-# async def main():
-#     # Load keypair from walletTest.json
-#     # with open("walletTest.json", "r") as file:
-#     #     keypair_data = json.load(file)[0]
-
-#     # public_key = keypair_data["public_key"]
-#     # private_key_base58 = keypair_data["private_key"]
-#     keypair = Keypair.from_base58_string('2Tojm2pUxXe4KPfnbzNwKWXb5jjT7J91FHfZ9qKwyx8MaSEiwRztSFuf9oV69BsGj7j5g9H6dPzhfe8u8Tspzdwh')
-#     private_key = keypair.secret()
-
-#     # private_key = Keypair.from_bytes(base58.b58decode(private_key_base58))
-    
-#     # Solana RPC endpoint URL
-#     solana_rpc_url = "https://api.mainnet-beta.solana.com"
-
-#     async_client = AsyncClient(solana_rpc_url)
-#     jupiter = Jupiter(
-#         async_client=async_client,
-#         keypair=keypair,
-#         quote_api_url="https://quote-api.jup.ag/v6/quote?",
-#         swap_api_url="https://quote-api.jup.ag/v6/swap",
-#         open_order_api_url="https://jup.ag/api/limit/v1/createOrder",
-#         cancel_orders_api_url="https://jup.ag/api/limit/v1/cancelOrders",
-#         query_open_orders_api_url="https://jup.ag/api/limit/v1/openOrders?wallet=",
-#         query_order_history_api_url="https://jup.ag/api/limit/v1/orderHistory",
-#         query_trade_history_api_url="https://jup.ag/api/limit/v1/tradeHistory"
-#     )
-
-#     async def execute_swap(input_mint: str, output_mint: str, amount: int, slippage_bps: int):
-#         max_retries = 3
-#         for attempt in range(max_retries):
-#             try:
-#                 transaction_data = await jupiter.swap(
-#                     input_mint=input_mint,
-#                     output_mint=output_mint,
-#                     amount=amount,
-#                     slippage_bps=slippage_bps,
-#                 )
-#                 break
-#             except httpx.ReadTimeout as e:
-#                 if attempt < max_retries - 1:
-#                     print(f"ReadTimeout occurred, retrying {attempt + 1}/{max_retries}...")
-#                     await asyncio.sleep(1)  # Wait before retrying
-#                 else:
-#                     print("Max retries reached. Exiting.")
-#                     return
-#             except httpx.UnsupportedProtocol as e:
-#                 print(f"UnsupportedProtocol error: {e}")
-#                 return
-#             except Exception as e:
-#                 print(f"Unexpected error during swap: {e}")
-#                 return
-
-#         try:
-#             raw_transaction = VersionedTransaction.from_bytes(base64.b64decode(transaction_data))
-#             signature = keypair.sign_message(message.to_bytes_versioned(raw_transaction.message))
-#             signed_txn = VersionedTransaction.populate(raw_transaction.message, [signature])
-#             opts = TxOpts(skip_preflight=False, preflight_commitment=Processed)
-#             result = await async_client.send_raw_transaction(txn=bytes(signed_txn), opts=opts)
-#             transaction_id = json.loads(result.to_json())['result']
-#             print(f"Transaction sent: https://solscan.io/tx/{transaction_id}")
-#             await check_transaction_status(transaction_id)
-#         except Exception as e:
-#             print(f"Error sending transaction: {e}")
-
-#     async def check_transaction_status(transaction_id: str):
-#         max_checks = 5
-#         for attempt in range(max_checks):
-#             try:
-#                 response = await async_client.get_signature_statuses([Signature.from_string(transaction_id)])
-#                 status = response.value[0]
-#                 if status is not None:
-#                     print(f"Transaction status: {status}")
-#                     break
-#                 else:
-#                     print(f"Transaction status not found, retrying {attempt + 1}/{max_checks}...")
-#                     await asyncio.sleep(5)  # Wait before retrying
-#             except Exception as e:
-#                 print(f"Error checking transaction status: {e}")
-
-#     # Define your token, slippage, and amount of SOL to swap
-#     input_token = "So11111111111111111111111111111111111111112"  # Example input token (SOL)
-#     output_token = "9EYScpiysGnEimnQPzazr7Jn9GVfxFYzgTEj85hV9L6U"  # Example output token
-#     swap_amount = 1_0  # 0.01 SOL in lamports
-#     slippage = 100  # 1% slippage in basis points
-
-#     # Execute the swap
-#     await execute_swap(input_token, output_token, swap_amount, slippage)
-
-
-# Run the main function
-# asyncio.run(main())
 
 
 class JupiterHelper():
@@ -147,7 +54,7 @@ class JupiterHelper():
             try:
                 transaction_data = await self.jupiter.swap(
                     input_mint=constant.input_mint,
-                    output_mint=str(output_mint),
+                    output_mint=output_mint,
                     amount=amount,
                     slippage_bps=slippage_bps,
                 )
@@ -194,15 +101,106 @@ class JupiterHelper():
             except Exception as e:
                 print(f"Error checking transaction status: {e}")
 
+    async def create_order(self, input_mint, output_mint, in_amount, out_amount, sender):        
+        max_retries = 3
+        for attempt in range(max_retries):
+            try:
+                transaction_data = await self.jupiter.open_order(input_mint, output_mint, in_amount, out_amount)
+                print("transaction_data>>>>>>>>>>>>>>", transaction_data)
+                break
+            except httpx.ReadTimeout as e:
+                if attempt < max_retries - 1:
+                    print(f"ReadTimeout occurred, retrying {attempt + 1}/{max_retries}...")
+                    await asyncio.sleep(1)  # Wait before retrying
+                else:
+                    print("Max retries reached. Exiting.")
+                    return ""
+            except httpx.UnsupportedProtocol as e:
+                print(f"UnsupportedProtocol error: {e}")
+                return ""
+            except Exception as e:
+                print(f"Unexpected error during swap: {e}")
+                return ""
 
-# helper = JupiterHelper()
-# sender = Keypair.from_base58_string("")
+        try:
+            raw_transaction = VersionedTransaction.from_bytes(base64.b64decode(transaction_data['transaction_data']))
+            signature = sender.sign_message(message.to_bytes_versioned(raw_transaction.message))
+            signed_txn = VersionedTransaction.populate(raw_transaction.message, [signature, transaction_data['signature2']])
+            opts = TxOpts(skip_preflight=False, preflight_commitment=Processed)
+            result = await self.async_client.send_raw_transaction(txn=bytes(signed_txn), opts=opts)
+            transaction_id = json.loads(result.to_json())['result']
+            return transaction_id
+        except Exception as e:
+            print(f"Error sending transaction: {e}")
+            return ""
 
-# helper.initializeJup(sender)
-# input_token = "So11111111111111111111111111111111111111112"  # Example input token (SOL)
-# output_token = ""  # Example output token
-# swap_amount = 1_0  # 0.01 SOL in lamports
-# slippage = 100  # 1% slippage in basis points
+    async def query_orders_history(self, wallet_address: str):
+        try:
+            # trade_history = await self.jupiter.query_trades_history(wallet_address)
+            # order_history = await self.jupiter.query_orders_history(wallet_address)
+            open_history = await self.jupiter.query_open_orders(wallet_address)
+            # print('history>>>>>>>>>>>>>>>>>>>', trade_history)
+            # print('order_history>>>>>>>>>>>>>>>>>>>', order_history)
+            # print('open_history>>>>>>>>>>>>>>>>>>>', open_history)
+            return open_history
+        except Exception as e:
+            print(f"Error sending transaction: {e}")
+            return ""
 
-# # helper.execute_swap(output_token, swap_amount, slippage, sender)
-# asyncio.run(helper.execute_swap(output_token, swap_amount, slippage, sender))
+    async def cancel_orders(self, orderList, sender):        
+        max_retries = 3
+        for attempt in range(max_retries):
+            try:
+                transaction_data = await self.jupiter.cancel_orders(orderList)
+                print("transaction_data>>>>>>>>>>>>>>", transaction_data)
+                break
+            except httpx.ReadTimeout as e:
+                if attempt < max_retries - 1:
+                    print(f"ReadTimeout occurred, retrying {attempt + 1}/{max_retries}...")
+                    await asyncio.sleep(1)  # Wait before retrying
+                else:
+                    print("Max retries reached. Exiting.")
+                    return ""
+            except httpx.UnsupportedProtocol as e:
+                print(f"UnsupportedProtocol error: {e}")
+                return ""
+            except Exception as e:
+                print(f"Unexpected error during swap: {e}")
+                return ""
+
+        try:
+            raw_transaction = VersionedTransaction.from_bytes(base64.b64decode(transaction_data))
+            signature = sender.sign_message(message.to_bytes_versioned(raw_transaction.message))
+            signed_txn = VersionedTransaction.populate(raw_transaction.message, [signature])
+            opts = TxOpts(skip_preflight=False, preflight_commitment=Processed)
+            result = await self.async_client.send_raw_transaction(txn=bytes(signed_txn), opts=opts)
+            transaction_id = json.loads(result.to_json())['result']
+            print(f"Transaction sent: https://solscan.io/tx/{transaction_id}")
+            return transaction_id
+        except Exception as e:
+            print(f"Error sending transaction: {e}")
+            return ""
+
+
+
+
+
+
+
+    def get_token_decimal_info(self, token_address):
+            try:
+                api_url = f"https://tokens.jup.ag/token/{token_address}"
+                response = requests.get(api_url)
+                response.raise_for_status()  # Check for HTTP errors
+                data = response.json()
+                print("data>>>>>>>>>>>>>>>", data)
+                if(data['decimals']):
+                    return data['decimals']
+                else:
+                    return None
+            except requests.exceptions.HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err}")
+                return None
+            except Exception as err:
+                print(f"Other error occurred: {err}")
+                return None
